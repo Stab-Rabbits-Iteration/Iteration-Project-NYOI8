51827c0fdddb34e826ec05f9112b63d0e0b44606
Darryl <hjdjoo@gmail.com>
2023-11-11 16:09:17 -0500

    Odin worked on setting up React routers. Darryl worked on components and styling.

--- client/containers/mainContainer.jsx
+++ client/containers/mainContainer.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
 
-import LoginContainer from './loginContainer.jsx';
-import SignupContainer from "./signupContainer.jsx";
-import HomeContainer from "./homeContainer.jsx";
+import LoginContainer from './LoginContainer.jsx';
+import SignupContainer from "./SignupContainer.jsx";
+import HomeContainer from "./HomeContainer.jsx";
 
 
 const MainContainer = () => {
 
   return (
     <div className="mainContainer">
+      <header id="pageHeader"><em>Peau Humaine</em></header>
       {/* Either input container or home container goes here... may make sense to use react router to render different components */}
       <Routes>
         // route base: loginContainer
+        <Route path="/" element={<HomeContainer />} />
         <Route path='/login' element={<LoginContainer />} />;
         // route 2: if no account or password is wrong
         // signupContainer
         // does this login need a slash or no?? Saw online that slash means that one loads first, but just to confirm
         <Route path='/signup' element={<SignupContainer />} />
       // after the sign up or login
-        // route to homeContainer
-        <Route path='/' element={<HomeContainer />} />
+      // route to homeContainer
       </Routes>
     </div>
   )
