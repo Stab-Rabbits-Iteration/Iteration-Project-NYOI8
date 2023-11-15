b0ad85ed512642ac1e2e2a11a9ccd93b8e4b218f
GiovanniCortes <giocortes19@gmail.com>
2023-11-15 16:01:01 -0500

    deleted old code comments

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -25,20 +25,4 @@ router.post(
   }
 );
 
-// ///Old Code/////////
-// router.get('/signup', (req, res) => {
-//   // this function serves the signup html when directed to the signup page
-//   res.sendFile(path.resolve(__dirname, '../client.signup.html'));
-// });
-
-// router.post('/signup', clientController.createClient, (req, res) => {
-//   console.log('client created');
-//   res.redirect('/home');
-// });
-
-// // after logging in, our plan is to have a component to display userinfo so the endpoint would be '/'?
-// router.get('/login', clientController.getClientInfo, (req, res) => {
-//   res.status(200).send(res.locals.clientObj);
-// });
-
 module.exports = router;
